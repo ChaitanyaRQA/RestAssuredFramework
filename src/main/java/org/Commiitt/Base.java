5bf62eddb09c25c75af3ccafbd7bcaf6508d9a19
ChaitanyaRQA <chaitanya.mudamala@gmail.com>
2025-04-18 16:59:41 -0400

    Initial base class created

---
+++ src/main/java/org/Commiitt/Base.java
@@ -0,0 +1,9 @@
+package org.Commiitt;
+
+public class Base {
+
+    public static void  main(String args[])
+    {
+        System.out.println("Welcome to RestAssured");
+    }
+}
---
+++ src/test/java/Testclass.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.*;
+
+public class Testclass {
+
+    //this is my testclass
+
+    @Test
+    public void display()
+    {
+
+        System.out.println("This is child class");
+    }
+}
